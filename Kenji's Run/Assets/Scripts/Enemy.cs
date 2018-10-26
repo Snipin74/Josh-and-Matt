d99c9a7613d1f8f48491f9381643a9b65504ce9b
mattwbeaton <44066260+mattwbeaton@users.noreply.github.com>
2018-10-26 01:09:11 -0400

    Enemy Zombies Moving

--- Kenji's Run/Assets/Scripts/Enemy.cs
+++ Kenji's Run/Assets/Scripts/Enemy.cs
@@ -4,6 +4,50 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour {
 
+    public float moveSpeed;
+    public bool moveRight;
+
+    public Transform wallCheck;
+    public float wallCheckRadius;
+    public LayerMask whatIsWall;
+    private bool hittingWall;
+
+    private bool notAtEdge;
+    public Transform EdgeCheck;
+    private Rigidbody2D myRidgidBody; // Player's RidgidBody
+
+
+
+    // Use this for initialization
+    void Start () {
+
+        myRidgidBody = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        hittingWall = Physics2D.OverlapCircle(wallCheck.position, wallCheckRadius, whatIsWall);
+        notAtEdge = Physics2D.OverlapCircle(EdgeCheck.position, wallCheckRadius, whatIsWall);
+
+        if (hittingWall || !notAtEdge)
+        {
+            moveRight = !moveRight;
+        }
+
+        if (moveRight)
+        {
+            transform.localScale = new Vector3(-0.64f, 0.5f, transform.localScale.z);
+            myRidgidBody.velocity = new Vector2(moveSpeed, myRidgidBody.velocity.y);
+
+        } else
+        {
+            transform.localScale = new Vector3(0.64f, 0.5f, transform.localScale.z);
+            myRidgidBody.velocity = new Vector2(-moveSpeed, myRidgidBody.velocity.y);
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.name.Equals("Sword"))
@@ -11,14 +55,4 @@ public class Enemy : MonoBehaviour {
             Destroy(gameObject);
         }
     }
-
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
