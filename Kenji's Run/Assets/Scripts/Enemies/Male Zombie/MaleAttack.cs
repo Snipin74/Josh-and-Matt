bbfd960ea2b87512762b82429514bcfbd934b544
mattwbeaton <44066260+mattwbeaton@users.noreply.github.com>
2018-10-26 12:58:48 -0400

    Female zombie attacks and kills player

---
+++ Kenji's Run/Assets/Scripts/Enemies/Male Zombie/MaleAttack.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaleAttack : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Equals("Player"))
+        {
+            MaleZombieController.IsAttacking = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Equals("Player"))
+        {
+            MaleZombieController.IsAttacking = false;
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+}
---
+++ Kenji's Run/Assets/Scripts/Enemies/Male Zombie/MaleZombieController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaleZombieController : MonoBehaviour {
+
+    public float moveSpeed;
+    public bool moveRight;
+
+    public Transform wallCheck;
+    public float wallCheckRadius;
+    public LayerMask whatIsWall;
+    private bool hittingWall;
+
+    private bool notAtEdge;
+    public Transform EdgeCheck;
+    private Rigidbody2D myRidgidBody; // Player's RidgidBody
+    private Animator myAnimator;
+
+    public static bool IsAttacking = false;
+    //internal static bool IsAttacking;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        myRidgidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        hittingWall = Physics2D.OverlapCircle(wallCheck.position, wallCheckRadius, whatIsWall);
+        notAtEdge = Physics2D.OverlapCircle(EdgeCheck.position, wallCheckRadius, whatIsWall);
+
+        if (hittingWall || !notAtEdge)
+        {
+            moveRight = !moveRight;
+        }
+
+        if (moveRight)
+        {
+            transform.localScale = new Vector3(-0.64f, 0.5f, transform.localScale.z);
+            myRidgidBody.velocity = new Vector2(moveSpeed, myRidgidBody.velocity.y);
+
+        }
+        else
+        {
+            transform.localScale = new Vector3(0.64f, 0.5f, transform.localScale.z);
+            myRidgidBody.velocity = new Vector2(-moveSpeed, myRidgidBody.velocity.y);
+        }
+
+        if (IsAttacking)
+        {
+            myAnimator.SetBool("Attack", true);
+            myRidgidBody.velocity = Vector2.zero;
+        }
+        else
+            myAnimator.SetBool("Attack", false);
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Equals("Sword"))
+        {
+            Destroy(gameObject);
+        }
+
+        /* if (collision.gameObject.name.Equals("Player"))
+         {
+             Enemy.IsAttacking = true;
+             Destroy(gameObject);
+         }*/
+    }
+}
