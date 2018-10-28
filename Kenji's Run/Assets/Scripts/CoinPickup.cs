3030e072db3b94493221606cf0f9fa12877805d6
mattwbeaton <44066260+mattwbeaton@users.noreply.github.com>
2018-10-27 20:05:05 -0400

    Coin/NinjaStar/ScoreHUD
    Coin is able to be picked up and shows a score in the HUD for it and changed the ninja star

---
+++ Kenji's Run/Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour {
+
+    public int pointsToGive;
+
+    private ScoreManager scoreHandler;
+
+    //private AudioSource coinSound;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        scoreHandler = FindObjectOfType<ScoreManager>();
+        //coinSound = GameObject.Find("Coin").GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            scoreHandler.AddPoints(pointsToGive);
+            gameObject.SetActive(false);
+
+            //if (coinSound.isPlaying)
+           // {
+               // coinSound.Stop();
+               // coinSound.Play();
+           // }
+           // else
+           // {
+               // coinSound.Play();
+           // }
+        }
+    }
+}
