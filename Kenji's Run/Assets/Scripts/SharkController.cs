321c738788199c2e99a81ed77ed1d98cd9580449
Snipin74 <josh.cove74@gmail.com>
2018-10-29 22:08:57 -0400

    shark and water
    sharks swimming and the level is a little more done

---
+++ Kenji's Run/Assets/Scripts/SharkController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SharkController : MonoBehaviour {
+
+    // [SerializeField] private float speed;
+
+    //public Transform target;
+    // public Transform target1;
+
+    public LayerMask enemyMask;
+    public float speed;
+    Rigidbody2D myBody;
+    Transform myTrans;
+    float myWidth;
+
+    // Use this for initialization
+    void Start () {
+        myTrans = this.transform;
+        myBody = this.GetComponent<Rigidbody2D>();
+        myWidth = this.GetComponent<SpriteRenderer>().bounds.extents.x;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        //check for the ground infront of the shark
+        Vector2 lineCastPos = myTrans.position - myTrans.right * myWidth;
+        Debug.DrawLine(lineCastPos, lineCastPos + Vector2.down);
+        bool isGrounded = Physics2D.Linecast(lineCastPos, lineCastPos + Vector2.down, enemyMask);
+
+        if (!isGrounded)
+        {
+            Vector3 currRot = myTrans.eulerAngles;
+            currRot.y += 180;
+            myTrans.eulerAngles = currRot;
+        }
+        //always move forward 
+        Vector2 myVel = myBody.velocity;
+        myVel.x = -myTrans.right.x * speed;
+        myBody.velocity = myVel;
+
+
+        //float step = speed * Time.deltaTime;
+
+        //transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+
+       
+    }
+}
